bd5b170b075babe5925d57ba68cd8c38871bebab
samwatson36 <sam.watson@kainos.com>
2026-08-05 14:22:26 +0100

    Setting-up a basic node backend for our team project
    AC criteria fulfilled including:
     running npm run build, nom run dev, and nom run start
     express installed
     app has one /health endpoint that returns status "UP" and the current time
    
    .

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { time } from "console";
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.json());
+
+/*
+
+// Root endpoint
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to your API!" });
+});
+
+*/
+
+// Health endpoint
+app.get("/health", (req, res) => {
+  res.json({ status: "UP", time: new Date().toISOString() });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Health Endpoint of Basic Node: http://localhost:${PORT}/health`);
+});
