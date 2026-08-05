8912c5881c00c6559ee82e5c457ace1d82d81f7f
Cillian2006 <cillian2055@gmail.com>
2026-08-05 15:26:50 +0100

    Add biome linting checks

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { time } from "console";
 import express from "express";
 
 const app = express();
@@ -8,11 +7,11 @@ const PORT = 3000;
 app.use(express.json());
 
 // Health endpoint
-app.get("/health", (req, res) => {
-  res.json({ status: "UP", time: new Date().toISOString() });
+app.get("/health", (_req, res) => {
+	res.json({ status: "UP", time: new Date().toISOString() });
 });
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Health Endpoint of Basic Node: http://localhost:${PORT}/health`);
+	console.log(`Health Endpoint of Basic Node: http://localhost:${PORT}/health`);
 });
