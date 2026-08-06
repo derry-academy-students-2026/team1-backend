f5a658fa7dfb07fe5502383cf0a70de7a014903c
chloe-mcc <chloe.mccarter@kainos.com>
2026-08-06 15:01:20 +0100

    Excluded coverage and made lint fixes

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,13 +1,13 @@
-import { app } from "../src/app.js";
-import { describe, it, expect } from "vitest";
 import request from "supertest";
+import { describe, expect, it } from "vitest";
+import { app } from "../src/app.js";
 
 describe("GET /health", () => {
-  it("should return status UP and current time", async () => {
-    const response = await request(app).get("/health");
+	it("should return status UP and current time", async () => {
+		const response = await request(app).get("/health");
 
-    expect(response.status).toBe(200);
-    expect(response.body.status).toBe("UP");
-    expect(new Date(response.body.time).toString()).not.toBe("Invalid Date");
-  });
+		expect(response.status).toBe(200);
+		expect(response.body.status).toBe("UP");
+		expect(new Date(response.body.time).toString()).not.toBe("Invalid Date");
+	});
 });
